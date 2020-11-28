00362e2be2cb3e466a76ea3aa9c3938e3f672de7
Caio Fernandes <caiiooo@Hotmail.com>
2020-11-28 00:46:54 -0300

    ajutes

--- index3.js
+++ index3.js
@@ -7,7 +7,7 @@ var _ = require("lodash");
 var timeFlag = true;
 const FATOR = 9.66;
 board.on("ready", function() {
-  var relay = new five.Relay(17);
+  var relay = new five.Relay(0);
   var pin = new five.Pin(27);
   // var button = new five.Button(10);
   var flowCount = 0;
